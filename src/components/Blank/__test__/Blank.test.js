44e270d07ad361a2e88afc7383d5a93d6994e414
chaowarat <chaowaratt@gmail.com>
2018-09-18 16:40:01 +0700

    add snapshot testing example

--- src/components/Blank/__test__/Blank.test.js
+++ src/components/Blank/__test__/Blank.test.js
@@ -2,20 +2,25 @@ import React from 'react'
 import Blank from '../Blank'
 import { shallow, mount, render } from 'enzyme'
 
-describe('black component test suite', function () {
+describe('black component test suite', () => {
   it('should render without throwing an error', () => {
     expect(shallow(<Blank />).contains(<div className="blank-container">Blank Component</div>)).toBe(true)
   })
 
-  it('should be selectable by class "blank-container"', function () {
+  it('should be selectable by class "blank-container"', () => {
     expect(shallow(<Blank />).is('.blank-container')).toBe(true)
   })
 
-  it('should mount in a full DOM', function () {
+  it('should mount in a full DOM', () => {
     expect(mount(<Blank />).find('.blank-container').length).toBe(1)
   })
 
-  it('should render to static HTML', function () {
+  it('should render to static HTML', () => {
     expect(render(<Blank />).text()).toEqual('Blank Component')
   })
+})
+
+describe('black component test suite with snapshot', () => {
+  const wrapper = shallow(<Blank />)
+  expect(wrapper).toMatchSnapshot()
 })
